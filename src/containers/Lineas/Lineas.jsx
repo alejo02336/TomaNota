6386f576994b315cb437dc01a75254a8d55cb58e
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-28 14:12:34 -0500

    finish lineas

--- src/containers/Lineas/Lineas.jsx
+++ src/containers/Lineas/Lineas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Lineas.css";
-import SearchBarZone from '../../components/SearchBarZone.jsx';
-import InfoContainer from '../../components/InfoContainer';
+import SearchBarZone from "../../components/SearchBarZone.jsx";
+import InfoContainer from "../../components/InfoContainer";
 
 export default function Lineas() {
   return (
@@ -14,24 +14,18 @@ export default function Lineas() {
           description="El Águila - Ansermanuevo - El Cairo - Ulloa - Argelia - Alcalá - Cartago - Obando"
           Ncomment="310 561 0381 - Omar Gómez Celiz"
         />
-
-        <InfoContainer
-          userName="Espacios seguros"
-          description="A la vuelta de la esquina"
-          Ncomment="Link"
-        />
       </section>
-      <section>
-        <section>
-          <img
-            src={require("./Lineas.svg")}
-            className="img-lineas"
-            alt="example"
-          />
+      <section className="report-container">
+        <section className="container-img-subforos">
+          <section>
+            <img
+              src={require("./Lineas(2).svg")}
+              className="img-lineas"
+              alt="example"
+            />
+          </section>
         </section>
       </section>
     </section>
-
-
   );
 }
