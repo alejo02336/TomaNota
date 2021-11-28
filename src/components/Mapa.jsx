9a0a6560822c76b4cfc70c001827ab2ae3a46bee
damddev <alejandrom02336@gmail.com>
2021-11-28 14:00:02 -0500

    circulos en el mapa

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,12 +1,29 @@
-import React, { Component } from "react";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-import SearchBar from "./SearchBar";
-import Btn from "./Btn";
-import "./styles/Search.css";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import {
+  Map,
+  InfoWindow,
+  Marker,
+  GoogleApiWrapper,
+  Polyline,
+  Circle,
+} from 'google-maps-react';
+import SearchBar from './SearchBar';
+import Btn from './Btn';
+import './styles/Search.css';
+import { Link } from 'react-router-dom';
 
 export class MapContainer extends Component {
   render() {
+    const coords = { lat: 3.384273, lng: -76.511593 };
+    const coords1 = { lat: 3.386984, lng: -76.514246 };
+    const coords2 = { lat: 3.37988, lng: -76.51523 };
+
+    const triangleCoords = [
+      { lat: 25.774, lng: -80.19 },
+      { lat: 18.466, lng: -66.118 },
+      { lat: 32.321, lng: -64.757 },
+      { lat: 25.774, lng: -80.19 },
+    ];
     return (
       <>
         <SearchBar />
@@ -14,10 +31,52 @@ export class MapContainer extends Component {
         <Map
           google={this.props.google}
           disableDefaultUI={true}
-          zoom={14}
+          zoom={15}
           fullscreenControl={false}
+          initialCenter={{
+            lat: 3.384273,
+            lng: -76.511593,
+          }}
         >
-          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+          <Marker onClick={this.onMarkerClick} name={'Current location'} />
+          <Circle
+            radius={200}
+            center={coords}
+            onMouseover={() => console.log('mouseover')}
+            onClick={() => console.log('click')}
+            onMouseout={() => console.log('mouseout')}
+            strokeColor="transparent"
+            strokeOpacity={0}
+            strokeWeight={5}
+            fillColor="#48F16D"
+            fillOpacity={0.5}
+          />
+
+          <Circle
+            radius={350}
+            center={coords1}
+            onMouseover={() => console.log('mouseover')}
+            onClick={() => console.log('click')}
+            onMouseout={() => console.log('mouseout')}
+            strokeColor="transparent"
+            strokeOpacity={0}
+            strokeWeight={5}
+            fillColor="#FF4949"
+            fillOpacity={0.5}
+          />
+
+          <Circle
+            radius={300}
+            center={coords2}
+            onMouseover={() => console.log('mouseover')}
+            onClick={() => console.log('click')}
+            onMouseout={() => console.log('mouseout')}
+            strokeColor="transparent"
+            strokeOpacity={0}
+            strokeWeight={5}
+            fillColor="#FFBD69"
+            fillOpacity={0.5}
+          />
         </Map>
         <Btn />
         <button className="infobtn">
@@ -31,5 +90,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyCq3TB-7rvTHsP8VBNn-dRwdPgHFNhfCZ0",
+  apiKey: 'AIzaSyCq3TB-7rvTHsP8VBNn-dRwdPgHFNhfCZ0',
 })(MapContainer);
