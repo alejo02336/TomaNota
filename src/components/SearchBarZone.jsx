9db422e2874323a95bff582aa977bb501ec5d280
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-28 13:29:47 -0500

    search bar zone style

--- src/components/SearchBarZone.jsx
+++ src/components/SearchBarZone.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-//import './styles/Search.css';
+import React from "react";
+
+import "./styles/Search.css";
 
 export default function SearchBarZone() {
   return (
-    <div>
+    <section className="search-container">
       <input
         type="text"
         name="Search"
+        maxLength={45}
         placeholder="Escribe el nombre del departamento en el que te encuentras"
       />
-    </div>
+    </section>
   );
 }
