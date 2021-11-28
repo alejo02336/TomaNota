bafc697e9b7db856966f5e42d9933962f5cd2cd5
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-28 12:51:35 -0500

    connect report btn to a route

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import './styles/Search.css';
+import React from "react";
+import "./styles/Search.css";
+import { Link } from "react-router-dom";
 
 const Btn = () => {
-  return <button className="abajo font">Reporta un caso</button>;
+  return (
+    <Link to="/report">
+      <button className="abajo font">Reporta un caso</button>
+    </Link>
+  );
 };
 
 export default Btn;
